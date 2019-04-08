2a07ffffa4d657b1bdd3c2975f4ac2f740a6a8cb
Alexandru Marjina <alex1997a25@gmail.com>
2019-04-08 16:04:16 +0300

    Added Swagger2 Api Documentation http://localhost:9000/swagger-ui.html#

--- src/main/java/com/marjina/springbootrestapi/controllers/EventController.java
+++ src/main/java/com/marjina/springbootrestapi/controllers/EventController.java
@@ -2,6 +2,10 @@ package com.marjina.springbootrestapi.controllers;
 
 import com.marjina.springbootrestapi.entities.Event;
 import com.marjina.springbootrestapi.services.EventService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +15,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/events")
+@Api(value = "Event controller CRUD", description = "Operations to get/add/update/delete events")
 public class EventController {
 
     private final EventService eventService;
@@ -20,12 +25,24 @@ public class EventController {
         this.eventService = eventService;
     }
 
+    @ApiOperation(value = "View a list of available events", response = List.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping
     public List<Event> showAll(){
         return eventService.fetchAllEvents();
 
     }
 
+    @ApiOperation(value = "Get a event by ID(UUID)", response = Event.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved event"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @GetMapping(path = {"/{id}"})
     public ResponseEntity<Event> findById(@PathVariable UUID id){
         return eventService.getEventById(id)
@@ -33,16 +50,35 @@ public class EventController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @ApiOperation(value = "Add an event", response = Event.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully added event"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PostMapping
     public Event addEvent(@RequestBody Event event){
         return eventService.addEvent(event);
     }
+
+    @ApiOperation(value = "Update an event", response = Event.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated event"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @PutMapping
     public ResponseEntity<Event> updateEvent(@RequestBody Event event){
         return eventService.editEvent(event)
                 .map(edited-> ResponseEntity.ok().body(edited))
                 .orElse(ResponseEntity.notFound().build());
     }
+    @ApiOperation(value = "Delete an event by ID(UUID)")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully deleted event"),
+            @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    })
     @DeleteMapping(path = {"/{id}"})
     public ResponseEntity<?> deleteEvent(@PathVariable("id")UUID id){
         return eventService.deleteEvent(id)
--- src/main/java/com/marjina/springbootrestapi/entities/Event.java
+++ src/main/java/com/marjina/springbootrestapi/entities/Event.java
@@ -1,5 +1,7 @@
 package com.marjina.springbootrestapi.entities;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import org.hibernate.annotations.Type;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,19 +16,25 @@ import java.util.UUID;
 @Data
 @Entity
 @Table(name = "Events")
+@ApiModel(description = "All details about the Employee. ")
 public class Event {
 
     @Id
     @Type(type = "org.hibernate.type.PostgresUUIDType")
     @GeneratedValue
+    @ApiModelProperty(notes ="The database generated event ID")
     private UUID id;
 
+    @ApiModelProperty(notes ="Title of event")
     private String title;
 
+    @ApiModelProperty(notes ="Host city of event")
     private String city;
 
+    @ApiModelProperty(notes ="Event's place")
     private String place;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @ApiModelProperty(notes ="Event's date")
     private LocalDate date;
 }
